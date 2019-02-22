b18e65ae71995ed874070a19aa5d60ccc04bee71
cappellachristian <46422659+cappellachristian@users.noreply.github.com>
2019-02-22 13:50:16 +0100

    Add files via upload

---
+++ WEB-INF/src/mypkg/GestioneAppuntamenti.java
@@ -0,0 +1,39 @@
+package mypkg;
+ 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+ 
+public class GestioneAppuntamenti extends HttpServlet {
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      // Set the response message's MIME type
+      response.setContentType("text/html;charset=UTF-8");
+      // Allocate a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+ 
+      // Write the response message, in an HTML page
+      try {
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title>GestioneAppuntamenti</title></head>");
+         out.println("<body>");
+         out.println("<form action=/PrenotaColloquio> Last Name: <br> <input type=text name =LastName> <br> <br> <input type= submit value=Submit>");
+         out.println("</form>");// 
+         // Echo client's request information
+         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+         // Generate a random number upon each request
+         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+         out.println("</body>");
+         out.println("</html>");
+      } finally {
+         out.close();  // Always close the output writer
+      }
+   }
+}
